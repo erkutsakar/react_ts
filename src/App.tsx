459322d2d19670408e4ba503fd8a4f9d7cf10ac6
erkutsakar <erkut.sakar@hotmail.com>
2023-02-20 00:02:52 +0300

    beta gamma

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@
 import './App.css';
 
 function App() {
+  let name : string = "Erkut";
+  let age : number = 26;
+  let done : boolean = true;
+  let trial : number | string =  "deneme";
+
+
+  
+  let obj = {
+    name: "Deniz",
+    age: 10,
+    done: true
+  }
+
   return (
     <div className="App">
-     Erkut ile typescript
+     {name + " _ " + age }
     </div>
   );
 }
